8daa36c919bdf07012377c8600dd4517ce24902d
marcos <marcgabe@myuax.com>
2024-12-03 16:33:05 +0100

    Configuracion base
    Configuracion base

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.kapt) // Asegúrate de que esta línea esté incluida
+    id("com.google.gms.google-services") version libs.versions.googleServices.get() // Aplica el plugin de Google Services
 }
 
 android {
@@ -10,12 +11,15 @@ android {
 
     defaultConfig {
         applicationId = "com.example.marcosgb_parcial_2_3"
-        minSdk = 34
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -36,11 +40,31 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    sourceSets {
+        getByName("main") {
+            manifest.srcFile("src/main/AndroidManifest.xml")
+            java.srcDirs("src/main/java")
+            res.srcDirs("src/main/res")
+        }
+    }
+
+    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
 
+    implementation (libs.ui)
+    implementation (libs.material3)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +73,14 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+    //implementation(libs.androidx.room.runtime) // Dependencia de Room runtime
+    //kapt(libs.androidx.room.compiler) // Dependencia de Room compiler
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +88,28 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Firebase dependencies
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.messaging)
+    implementation(libs.androidx.multidex)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+
+    //extras
+    implementation (libs.androidx.work.runtime.ktx)
 }
+
+// Aplica el plugin de Google Services
+apply(plugin = "com.google.gms.google-services")
+
+/*testImplementation(libs.junit)
+testImplementation(libs.kotlinx.coroutines.test.v164)
+androidTestImplementation(libs.ui.test.junit4)
+androidTestImplementation(libs.androidx.junit.v113)
+androidTestImplementation(libs.androidx.espresso.core.v340)
+testImplementation(libs.kotlinx.coroutines.test)
+*/
+
+
